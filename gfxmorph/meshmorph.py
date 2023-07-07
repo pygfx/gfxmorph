123a485517f9d9f704a90dd0be2353c98cef1f0d
Almar Klein <almar@almarklein.org>
2023-07-07 23:17:52 +0200

    Can close slightly larger holes, kindof.

--- gfxmorph/meshmorph.py
+++ gfxmorph/meshmorph.py
@@ -440,6 +440,9 @@ class AbstractMesh:
             assert len(boundary) >= 3  # I don't think they can be smaller, right?
             if len(boundary) == 3:
                 new_faces.append(boundary)
+            elif len(boundary) == 4:
+                new_faces.append(boundary[:3])
+                new_faces.append(boundary[2:] + boundary[:1])
             else:
                 pass
                 # We can apply the earcut algororithm to fill larger
